bfc7a1e81de6b45f7d817d7abcfa0751fbaf8f78
Beatriz Rodríguez <116880473+Bea-ro@users.noreply.github.com>
2025-08-27 17:56:38 +0200

    add sitemap and meta tags

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TitleComponent } from './components/title/title';
 import { Info } from './components/info/info';
 import { Pains } from './components/pains/pains';
@@ -9,7 +9,6 @@ import { Faqs } from './components/faqs/faqs';
 import { Contact } from './components/contact/contact';
 import { CardText, CardTextAndDetails } from './models/cardTexts.models';
 import { pricesCards, reasonsCards } from './data/cardTexts';
-import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -18,29 +17,12 @@ import { Meta, Title } from '@angular/platform-browser';
   styleUrl: './app.css'
 })
 export class App implements OnInit {
-  protected readonly title = signal('tws');
-
   public reasonsCards: CardText[][] = reasonsCards;
   public pricesCards: CardTextAndDetails[] = pricesCards;
   public pricesCardsTexts: CardText[][] = [];
   public content: string = '';
 
-  constructor(
-    private meta: Meta,
-    private titleService: Title
-  ) {
-    this.pricesCards.map((priceCard) => this.pricesCardsTexts.push(priceCard.text));
-  }
   ngOnInit() {
-    this.titleService.setTitle('Tu web solidaria | Diseño web para ONG');
-    this.meta.updateTag({
-      name: 'description',
-      content: 'Diseño web especializado en ONG. Colaborando con otras ONG. Infórmate.'
-    });
-    this.meta.updateTag({
-      name: 'keywords',
-      content: 'diseño web, web solidaria'
-    });
-    this.meta.updateTag({ name: 'robots', content: 'index, follow' });
+    this.pricesCards.map((priceCard) => this.pricesCardsTexts.push(priceCard.text));
   }
 }
